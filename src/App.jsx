249d284eb5d8e8872b19577342be3d40e8e80523
Adameia Ramsey <adameiaproductions@gmail.com>
2024-03-04 14:55:19 -0600

    saving progress

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,28 @@ import "./App.css"
 import { CustomerList } from "./components/customers/CustomerList.jsx"
 import { TicketList } from "./components/tickets/TicketList.jsx"
 import { StaffList } from "./components/staff/StaffList.jsx"
+import { Routes, Route } from "react-router-dom"
+import { NavBar } from "./components/nav/NavBar.jsx"
+import { Outlet } from "react-router-dom"
+import { Welcome } from "./welcome/Welcome.jsx"
 
 export const App = () => {
-  return <>
-    {/* <TicketList /> */}
-    <CustomerList />
-    <StaffList />
-  </>
+  return (<Routes>
+    <Route path="/" element=
+      {
+        <>
+          <NavBar />
+          <Outlet /> {/* child routes render where Outlet is called */}
+        </>
+      }
+    >
+      <Route index element={<Welcome />} /> {/* the "index" route allows you to add an element that is only rendered on the parent
+      page, which in this case is the home page of the site. If I were to put "Welcome" between NavBar and Outlet, it would render
+      on every page.*/}
+      <Route path="tickets" element={<TicketList />} />
+      <Route path="customers" element={<CustomerList />} />
+      <Route path="staff" element={<StaffList />} />
+    </Route>
+  </Routes>
+  )
 }
---
+++ src/welcome/Welcome.jsx
@@ -0,0 +1,10 @@
+import "./Welcome.css"
+
+export const Welcome = () => {
+    return (
+        <div className="welcome-container">
+            <h1><span>Welcome to</span><span>Honey Rae's Rapier Shop</span></h1>
+            <div>Or, alternatively, Welcome to Unknown Death 2002 because I have terrible taste in music</div>
+        </div>
+    )
+}
